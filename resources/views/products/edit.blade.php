fc5e5845bd7f7d67932d5b9b323c49b13265ccbe
RaulCisneros67 <cisnerosraul139@gmail.com>
2023-08-09 19:58:08 -0600

    Cambiar añadir

--- resources/views/products/edit.blade.php
+++ resources/views/products/edit.blade.php
@@ -38,7 +38,7 @@ placeholder="Marca" name="marca" value="{{ $product->Marca }}">
 placeholder="Precio" name="precio" value="{{ $product->precio }}">
 
 <button type="submit" class="my-3 w-full bg-green-500 p-2 font-semibold
-rounded text-white hover:bg-green-600">Agregar</button>
+rounded text-white hover:bg-green-600">Añadir</button>
 
 </form>
 
