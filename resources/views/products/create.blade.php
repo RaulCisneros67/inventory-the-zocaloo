cb7fcd1430f4942cec0fc26e706fdbfe9851027d
RaulCisneros67 <cisnerosraul139@gmail.com>
2023-08-09 19:59:50 -0600

    Cambiar crear

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -14,7 +14,7 @@
 class="bg-white w-1/3 p-4 border-gray-100 shadow-xl rounded-lg">
 @csrf
 
-<h2 class="text-2xl text-center py-4 mb-4 font-semibold">Crear Productos</h2>
+<h2 class="text-2xl text-center py-4 mb-4 font-semibold">Crear </h2>
 
 <input class="my-2 w-full bg-gray-200 p-2 text-lg rounded placeholder-gray-900"
 placeholder="Imagen" name="file" type="file">
